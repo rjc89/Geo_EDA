3fa452268584e180a7495b8b660257580b8669dd
Robert <rjconstable1989@gmail.com>
2020-07-15 16:47:47 +0100

    updated example

--- README.md
+++ README.md
@@ -3,12 +3,18 @@
 ### A tool for geospatial visualisation
 
 ### Idea
-Create a simple web app enable the visualisation of geospatial datasets from tabulated data containing longitude and latitude columns and an optional GeoJSON 
+Create a simple web app enable the visualisation of geospatial datasets from tabulated data containing longitude and latitude columns 
 
 ### Usage
 
-Select the longitude and latitude columns and GeoJSON from the drop down and customise what is shown using the sidebar widgets
+Select the longitude and latitude columns from the drop down and customise what is shown and how using the sidebar widgets
+
+<img src="./example.png">
 
 ### Approach
 
-Plots are generated using plotly express 
+The map is generated using Plotly express with publically available USGS and weather layers. The web app was built with Streamlit
+
+### Extra complications to add
+
+GeoJSON chloropleth coloring support
--- webapp.py
+++ webapp.py
@@ -1,17 +1,39 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-from plots import plotly_ex_map
+# import plotly.graph_objects as go
+# import json
 
-us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+
 
 st.title('Geo_EDA')
-st.write(us_cities.head())
+st.write('https://github.com/rjc89/Geo_EDA')
+example_data = st.checkbox('Use example .csv')
 
-Sat_street = st.sidebar.checkbox("satellite-streets")
+#uploaded_file = st.file_uploader("Choose a dataset .csv", type="csv")
+#us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+#if uploaded_file:
+#data = pd.read_csv(uploaded_file)
+if example_data:
+    data = pd.read_csv("./us_cities_top_1k.csv")
+st.write(data.head(3))
 
-if Sat_street:
-    style = "satellite-streets"
+cols = list(data.columns)
+
+# GeoJSON = st.sidebar.file_uploader('Choose a GeoJSON')
+# locations = st.sidebar.selectbox('Choose a chloropleth color column', cols)
+#marker_color = st.sidebar.slider('marker color', )
+marker_size = st.sidebar.selectbox('marker size', cols)
+latitude = st.sidebar.selectbox('latitude', cols)
+longitude = st.sidebar.selectbox('longitude', cols)
+hover = st.sidebar.selectbox('hover label', cols)
+#hover_data = st.sidebar.multiselect('hover', cols)
+terrain = st.sidebar.checkbox('Terrain')
+
+#GeoJSON_data = json.load(GeoJSON)
+
+if terrain:
+    style = 'stamen-terrain'
 else:
     style = "open-street-map"
 
@@ -39,24 +61,18 @@ if USGS_box:
 if Weather_box:
     layers.append(weather)
 
-st.plotly_chart(plotly_ex_map(us_cities, style, layers))
+def plotly_ex_map(data, style, layers, latitude, longitude, hover, marker_size):
+    fig = px.scatter_mapbox(data, lat=latitude, lon=longitude, hover_name=hover, 
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=300, size = marker_size)
+    fig.update_layout(
+        mapbox_style="white-bg",
+        mapbox_layers= layers)
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_layout(mapbox_style=style)
+    return fig
 
+if example_data:
+    st.plotly_chart(plotly_ex_map(data, style, layers, data['lat'], data['lon'], data['City'], data['Population']))
+else:
+    st.plotly_chart(plotly_ex_map(data, style, layers, data[latitude], data[longitude], data[hover], data[marker_size]))
 
-
-
-
-# [
-#             {
-#                 "below": 'traces',
-#                 "sourcetype": "raster",
-#                 "source": [
-#                     "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
-#                 ]
-#             },
-#             {
-#                 "sourcetype": "raster",
-#                 "source": ["https://geo.weather.gc.ca/geomet/?"
-#                         "SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX={bbox-epsg-3857}&CRS=EPSG:3857"
-#                         "&WIDTH=1000&HEIGHT=1000&LAYERS=RADAR_1KM_RDBR&TILED=true&FORMAT=image/png"],
-#             }
-#         ]
