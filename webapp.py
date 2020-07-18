8a139f13d1074f1e31a25c96289b034c9b6cb172
Robert <rjconstable1989@gmail.com>
2020-07-18 15:23:58 +0100

    updates

--- webapp.py
+++ webapp.py
@@ -8,71 +8,93 @@ import plotly.express as px
 
 st.title('Geo_EDA')
 st.write('https://github.com/rjc89/Geo_EDA')
-example_data = st.checkbox('Use example .csv')
+example_data = st.checkbox('Use example .csv', value = True)
 
 #uploaded_file = st.file_uploader("Choose a dataset .csv", type="csv")
 #us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+
 #if uploaded_file:
 #data = pd.read_csv(uploaded_file)
+data = []
 if example_data:
     data = pd.read_csv("./us_cities_top_1k.csv")
-st.write(data.head(3))
+if example_data == False:
+    uploaded_file = st.file_uploader("Choose a dataset .csv", type="csv")
+    if uploaded_file:
+        data = pd.read_csv(uploaded_file)
 
-cols = list(data.columns)
+if len(data) != 0:
+    st.write(data.head(3))
 
-# GeoJSON = st.sidebar.file_uploader('Choose a GeoJSON')
-# locations = st.sidebar.selectbox('Choose a chloropleth color column', cols)
-#marker_color = st.sidebar.slider('marker color', )
-marker_size = st.sidebar.selectbox('marker size', cols)
-latitude = st.sidebar.selectbox('latitude', cols)
-longitude = st.sidebar.selectbox('longitude', cols)
-hover = st.sidebar.selectbox('hover label', cols)
-#hover_data = st.sidebar.multiselect('hover', cols)
-terrain = st.sidebar.checkbox('Terrain')
+    cols = list(data.columns)
+    marker_cols = list(data.columns)
+    marker_cols.insert(0, 'None')
+   
+    #st.write(cols, marker_cols)
 
-#GeoJSON_data = json.load(GeoJSON)
+    # GeoJSON = st.sidebar.file_uploader('Choose a GeoJSON')
+    # locations = st.sidebar.selectbox('Choose a chloropleth color column', cols)
+    #marker_color = st.sidebar.slider('marker color', )
+    marker_size_slider = st.sidebar.slider('marker size', 5, 20)
+    marker_size = st.sidebar.selectbox('marker size by', marker_cols)
+    latitude = st.sidebar.selectbox('latitude', cols)
+    longitude = st.sidebar.selectbox('longitude', cols)
+    hover = st.sidebar.selectbox('hover label', cols)
+    #hover_data = st.sidebar.multiselect('hover', cols)
+    terrain = st.sidebar.checkbox('Terrain')
 
-if terrain:
-    style = 'stamen-terrain'
-else:
-    style = "open-street-map"
+    #GeoJSON_data = json.load(GeoJSON)
 
-layers = []
+    if terrain:
+        style = 'stamen-terrain'
+    else:
+        style = "open-street-map"
 
-weather = {
-                "sourcetype": "raster",
-                "source": ["https://geo.weather.gc.ca/geomet/?"
-                        "SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX={bbox-epsg-3857}&CRS=EPSG:3857"
-                        "&WIDTH=1000&HEIGHT=1000&LAYERS=RADAR_1KM_RDBR&TILED=true&FORMAT=image/png"],
-            }
+    layers = []
 
-USGS = {
-                "below": 'traces',
-                "sourcetype": "raster",
-                "source": [
-                    "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
-                ]
-            }
-USGS_box = st.sidebar.checkbox("USGS")
-Weather_box = st.sidebar.checkbox("Weather")
+    weather = {
+                    "sourcetype": "raster",
+                    "source": ["https://geo.weather.gc.ca/geomet/?"
+                            "SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX={bbox-epsg-3857}&CRS=EPSG:3857"
+                            "&WIDTH=1000&HEIGHT=1000&LAYERS=RADAR_1KM_RDBR&TILED=true&FORMAT=image/png"],
+                }
 
-if USGS_box:
-    layers.append(USGS)
-if Weather_box:
-    layers.append(weather)
+    USGS = {
+                    "below": 'traces',
+                    "sourcetype": "raster",
+                    "source": [
+                        "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
+                    ]
+                }
+    USGS_box = st.sidebar.checkbox("USGS")
+    Weather_box = st.sidebar.checkbox("Weather")
 
-def plotly_ex_map(data, style, layers, latitude, longitude, hover, marker_size):
-    fig = px.scatter_mapbox(data, lat=latitude, lon=longitude, hover_name=hover, 
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=300, size = marker_size)
-    fig.update_layout(
-        mapbox_style="white-bg",
-        mapbox_layers= layers)
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    fig.update_layout(mapbox_style=style)
-    return fig
+    if USGS_box:
+        layers.append(USGS)
+    if Weather_box:
+        layers.append(weather)
 
-if example_data:
-    st.plotly_chart(plotly_ex_map(data, style, layers, data['lat'], data['lon'], data['City'], data['Population']))
-else:
-    st.plotly_chart(plotly_ex_map(data, style, layers, data[latitude], data[longitude], data[hover], data[marker_size]))
+    def plotly_ex_map(data, style, layers, latitude, longitude, hover, marker_size):
+        # if marker_size.empty:
+        #     marker_size = [marker_size_slider] * len(data)
+        # if marker_size == 'Population':
+        #     marker_size = marker_size_slider * data['Population']
+        # else:
+        #     marker_size = data[marker_size] * marker_size_slider
+
+        
+
+        fig = px.scatter_mapbox(data, lat=latitude, lon=longitude, hover_name=hover, 
+                            color_discrete_sequence=["fuchsia"], zoom=3, height=300, size = marker_size)
+        fig.update_layout(
+            mapbox_style="white-bg",
+            mapbox_layers= layers)
+        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        fig.update_layout(mapbox_style=style)
+        return fig
+
+    if example_data:
+        st.plotly_chart(plotly_ex_map(data, style, layers, data['lat'], data['lon'], data['City'], data['Population']))
+    else:
+        st.plotly_chart(plotly_ex_map(data, style, layers, data[latitude], data[longitude], data[hover], data[marker_size]))
 
